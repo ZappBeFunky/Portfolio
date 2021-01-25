cf9d4f99509b854d5fa9aac088a442052ca35016
Xavier Bergeron <xavbergeron@gmail.com>
2021-01-24 22:53:30 -0800

    Remove dist folder

--- dist/scripts/main.js
+++
@@ -1,9 +0,0 @@
-function scrollProjectsToMiddle() {
-  const projectList = document.getElementById("project-list")
-  let secondProject = projectList.querySelector(".project:nth-of-type(2)")
-  secondProject.scrollIntoView({ inline: "center" })
-}
-
-window.addEventListener("load", function () {
-  scrollProjectsToMiddle()
-})
